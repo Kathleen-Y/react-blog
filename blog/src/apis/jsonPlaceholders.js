6ee89a9ff4168c283ec2c301607e65ed7eb258d5
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-25 12:02:59 -0700

    Update jsonPlaceholders.js
    ideas

--- blog/src/apis/jsonPlaceholders.js
+++ blog/src/apis/jsonPlaceholders.js
@@ -4,3 +4,8 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com'
 })
+
+pseudo to why the issue, ideas.
+is the issue with exporting this page?
+    or with the fact that this json is a tester not a real REST API, 
+    and it returns "undefined" meaning that somthing is coming through but not the product bc it has not produce to return?....
